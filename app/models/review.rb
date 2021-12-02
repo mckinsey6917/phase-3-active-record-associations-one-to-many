73814d2dca0d481de0f39b35ad7f49dcf70372a4
mckinseyleckenby <leckenbym11@gmail.com>
2021-12-02 07:36:31 -0600

    commit tree 77b3ba0109d7068f4665e0cd1b26586350e0b957

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
-  
-end
+    #a review belongs to a game
+    # def game
+    #     # self is the review instance
+    #     Game.find(self.game_id)
+#OR JUST WRITE MACRO 
+    belongs_to :game
+    end
+
